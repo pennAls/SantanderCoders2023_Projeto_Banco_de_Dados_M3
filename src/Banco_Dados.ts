ffc95720963295d52effa0a29bf090758cb7671d
thiago <thiagoalbino2000@hotmail.com>
2024-03-11 10:34:09 -0300

    feat: criando os métodos AdicionarPessoas e ListarPessoas

--- src/Banco_Dados.ts
+++ src/Banco_Dados.ts
@@ -1,6 +1,38 @@
-import { Pessoa} from './Pessoa.ts';
+import { Pessoa } from "./Pessoa.ts";
 
 class Banco_Dados {
-    private pessoas: Pessoa[] = [];
+  private pessoas: Pessoa[] = [];
 
+  AdicionarPessoa(novaPessoa: Pessoa): string {
+    const pessoaExiste = this.pessoas.find(
+      (pessoa) => pessoa.Nome === novaPessoa.Nome
+    );
+    if (pessoaExiste) {
+      return `Já existe uma pessoa cadastrada com esse nome.`;
+    } else {
+      this.pessoas.push(novaPessoa);
+      return "Adicionado com sucesso.";
+    }
+}   
+
+    ListarPessoa():Pessoa[]{
+        return dados.Pessoas
+    }
+
+  get Pessoas(): Pessoa[] {
+    return this.pessoas;
+  }
 }
+
+const dados = new Banco_Dados();
+
+// const novaPessoa = new Pessoa("joao", 20, "12312312312");
+
+// const newPessoa = new Pessoa("joao", 18, "12312312312");
+
+// console.log(dados.AdicionarPessoa(novaPessoa));
+// console.log(dados.AdicionarPessoa(newPessoa));
+
+// console.log(dados.ListarPessoa())
+
+
